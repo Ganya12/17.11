15ac131c9eb34211bc3ae6b9f1f3f9748f22d5c8
Ganya12 <103940461+Ganya12@users.noreply.github.com>
2022-11-17 15:38:33 +0300

    Add files via upload

---
+++ app/request/app.js
@@ -0,0 +1,15 @@
+
+const express = require('express'),
+        router = express.Router(),
+        app = express();
+
+const host = '127.0.0.1';
+const port = 7000;
+
+router.get('/books', (req, res) => {
+    console.log(req.path);
+});
+
+app.use('/api', router);
+
+app.listen(port, host, () => console.log(`Server listens http://${host}:${port}`));
